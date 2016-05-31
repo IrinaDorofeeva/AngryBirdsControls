245919800dc1008dc7eb7f3af245e0c9f176e16d
Mac <Mac@mavericks-5.local>
2016-05-31 15:32:50 -0400

    Initial Commit

---
+++ SkutHW26ControlsPart2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SkutHW26ControlsPart2
+//
+//  Created by Mac on 5/31/16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SkutHW26ControlsPart2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SkutHW26ControlsPart2
+//
+//  Created by Mac on 5/31/16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
